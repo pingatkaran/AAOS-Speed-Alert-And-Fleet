26ad9234fae3ebd2bac5b148c061b32d58cf3498
Karan Mahboobani <pingatkaran@gmail.com>
2025-03-07 18:01:06 +0530

    Added AWS SNS

--- automotive/src/main/java/com/app/drivealert/SpeedService.kt
+++ automotive/src/main/java/com/app/drivealert/SpeedService.kt
@@ -16,9 +16,14 @@ import androidx.car.app.CarAppService
 import androidx.car.app.notification.CarAppExtender
 import androidx.car.app.validation.HostValidator
 import androidx.core.app.NotificationCompat
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.regions.Region
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.sns.AmazonSNSClient
+import com.amazonaws.services.sns.model.CreateTopicRequest
+import com.amazonaws.services.sns.model.PublishRequest
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.messaging.FirebaseMessaging
-import com.google.firebase.messaging.RemoteMessage
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -33,6 +38,12 @@ class SpeedService : CarAppService() {
     private var speedLimit: Int = 140
     private var name: String = "140"
     private val firebaseMessaging = FirebaseMessaging.getInstance()
+
+    private var isOverSpeedLimit = false
+
+    private lateinit var snsClient: AmazonSNSClient
+    private lateinit var snsTopicArn: String
+    private var isAwsInitialized = false
 
     companion object {
         private const val KM_MULTIPLIER = 3.59999987F // Use to convert miles to kms
@@ -53,10 +64,12 @@ class SpeedService : CarAppService() {
     override fun onCreate() {
         super.onCreate()
         initCar()
+        CoroutineScope(Dispatchers.IO).launch {
+            initAwsSns()
+        }
         registerCarPropertyCallback()
         createNotificationChannel()
 
-        // Subscribe to fleet notifications topic
         firebaseMessaging.subscribeToTopic(FLEET_TOPIC).addOnCompleteListener { task ->
             if (task.isSuccessful) {
                 Log.d("Firebase", "Subscribed to fleet notifications")
@@ -71,6 +84,23 @@ class SpeedService : CarAppService() {
         mCarPropertyManager = car.getCarManager(Car.PROPERTY_SERVICE) as CarPropertyManager
     }
 
+    private fun initAwsSns() {
+        try {
+            val credentials = BasicAWSCredentials(
+                "AKIA4WJPWR3Y6UKLQVNJ", "LHLZOrjIt222VSkwZmnoVq31hXXfS/tiC2BLxojn"
+            )
+            snsClient = AmazonSNSClient(credentials)
+            snsClient.setRegion(Region.getRegion(Regions.fromName("eu-north-1")))
+
+            snsTopicArn = "arn:aws:sns:eu-north-1:872515276529:fleet_notifications"
+            isAwsInitialized = true
+            Log.d("AWS-SNS", "Using known SNS topic ARN: $snsTopicArn")
+        } catch (e: Exception) {
+            Log.e("AWS-SNS", "Failed to initialize SNS client", e)
+            isAwsInitialized = false
+        }
+    }
+
     private fun registerCarPropertyCallback() {
         mCarPropertyManager.registerCallback(
             speedCallback,
@@ -83,9 +113,15 @@ class SpeedService : CarAppService() {
         override fun onChangeEvent(carPropertyValue: CarPropertyValue<*>) {
             val currentSpeed = ((carPropertyValue.value as Float) * KM_MULTIPLIER)
 
-            if (currentSpeed > speedLimit) {
+            if (currentSpeed > speedLimit && !isOverSpeedLimit) {
+                isOverSpeedLimit = true
                 showSpeedingNotification(currentSpeed)
                 sendFleetNotification(currentSpeed)
+                if (isAwsInitialized) {
+                    sendAwsSnsNotification(currentSpeed)
+                }
+            } else if (currentSpeed <= (speedLimit - 2) && isOverSpeedLimit) {
+                isOverSpeedLimit = false
             }
         }
 
@@ -101,20 +137,12 @@ class SpeedService : CarAppService() {
                 put("speedLimit", speedLimit.toString())
                 put("driverName", name)
             }
-
-            // Send notification via Firebase Cloud Messaging
-            val message = RemoteMessage.Builder(FLEET_TOPIC).addData("type", "speed_alert")
-                .addData("details", notificationData.toString()).build()
-
-            // Optional: Send via HTTP request for more control
             sendFCMNotificationViaHttp(notificationData)
-
         } catch (e: Exception) {
             Log.e("Firebase", "Error sending fleet notification", e)
         }
     }
 
-    // Replace sendFCMNotificationViaHttp with this
     private fun sendFCMNotificationViaHttp(notificationData: JSONObject) {
         CoroutineScope(Dispatchers.IO).launch {
             try {
@@ -151,22 +179,21 @@ class SpeedService : CarAppService() {
     private suspend fun getFcmAccessToken(): String {
         val serviceAccountJson = """
     {
-        "type": "service_account",
-        "project_id": "aaos-assignment",
-        "private_key_id": "2be1de406bb228022109a4ff0479b04751950120",
-        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCcuSXbdQR7YjNl\naNBKJ+4b3RKv0PxQ1TM//1YBLrxJBhw3BdoCvlz8I2TKN23tcdlrDAQv08tx8mFj\nPmqk1oyI00JnXkfzTcclplloDJJWojkRhJqyvgcW4oUGNzJoPSm7Yt2Ibp3ScIWk\nfVePBSt2p0TrnVlNzLERxMJA1XB9iiIwx0Vn+vZv/ePh2/aG769q+wrYTp19Nbit\nkJcGfqGW38YcFz7M9tPVd04O7dyZse4C+TLnd8eYCglpNNjeECjSs1qTIKqJBlsb\nnDmdWaH86YFhOLnpKYmrXW5Bug9d481V7JrUj9AvHNKS5JN/Ulz9qYk/IuLQz/LY\nm2WIIodnAgMBAAECggEACwPfi329ZrQ8a1U8xoMcWV/MldTfcKL7d8HV5Qr7qtPZ\nUDi2EPLmQCzddnHQGE89k8HqQJ5NmODYEKY5z2odwh273hE8IPdt454kf76nQZvw\n3OO82vE/0ejyQCfJZpaRHbceKoRzzTwHxWVJFFrg3VvctgUYjYvkdulx9owwQtjb\nuQBPK5rwLy2c85nIfg3ycVib/YrMYjKsjnl07vdXryQaGKRMLZ+y+bUbBKDwdGKf\nESPxyLtKQGHO/PmSTzwoFMui1MQDSwQIjoGpfesIgP6YBghtdRfDm4Z1keBZvPg9\njPEk3yV83rDo+TwBZ4CJtIR9tWXO0PLJJGJofoZkmQKBgQDafnKk0PG3ys1RfSRb\nmcau8SYvstU1XJySAwYK2akO/wCPRoWcQJbvB6yjMX6NRC8f/MhDWh+Oouc71gP2\nOB2R94b6B5QdgCFY7gSXc1U6cDkCNSBawlcbY19aPJXTWHMnRAg8zJQSNO83wRa7\nvwYumn9Slg7EvaDamt85lC+cFQKBgQC3oDubpfr5GbxDfsOr4BjpkqkWP/w3r7e1\nv0VR8KPOUrOEbFmwlJWoxNmwRU+jKOwy1+2RthQhUnU8pDjZtgcrLBwMC5uTZCU5\nWPEI/omnwxJpavYb3PVn0XQeeOFL4FZ8xA5wREDsfEkffyaWHpqzSrBadVoWpvmU\ncmcffC+oiwKBgQCbEd1p7kVvzQaVjDyuqmAhr1MLzV+WxMj2tCCouLxMwVzzUvjm\n4cx9jzPOiwUnW7oaeLzlWGRHRoTYqdcb+8242DW65IARw+tnA17M4nUdyaIcV/pD\nj+Rkss1s7MzlN8RanOU/yW5Z0anqJydsTnIjx1EQGyIWLgOowvwlEHAN1QKBgQCL\nGrBtTgTdFhjfjLVQjBhzQS4kc95ulQf3f+lUlJaS42WpCKe9NCJX0qASf78/m+2q\n6pbHDocbYyr7AHEsxE+T7ftlSJnXw0LLucwHA0RmuUlGYMgF/ZJOzoZH20zZPR5n\nr+I8PAhu3gxK9vjVOmP5NLh6HnJbvKxPP4dNe7di1wKBgCP1lqYyaAu8FLf2iHep\nvpHeZ1cjBcNqStGXjPouGFi0lMvT7LIegNyoisHnpOgamkghkBOxtwzDmFsmyA9g\nlEoFxE3N0CUd1TsRn6IhK56ThvRi9w7FgP+JpLW9fA33AdyNY/x7frigauN2Qod4\nmwcfLoq6aHtDpBNErmFCLRWw\n-----END PRIVATE KEY-----\n",
-        "client_email": "firebase-adminsdk-fbsvc@aaos-assignment.iam.gserviceaccount.com",
-        "client_id": "115002391646449095117",
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40aaos-assignment.iam.gserviceaccount.com",
-        "universe_domain": "googleapis.com"
-    }
+  "type": "service_account",
+  "project_id": "aaos-assignment",
+  "private_key_id": "892212b093771a2e14beb28d8f63fd848ec73d4d",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCA3aMRFcRICKeF\nOVToTJUyLSs1lhZCs6t8WUnWr/kh+5m0aBpjCNXmBPNRC3CYgPZB9pJzAB14FkHR\nLdGyhPHOFunsL3qYzwrYO4fsYQyhF4QlVQfyTBa5j7C1JTL2ER/pYsCD5D7mV4ez\n21nagK+x5xfBUSdSapkAJfrB9B+qB3N5iPBphv15XiNAxU0QdjRwulizi8vXcDyF\nh8gKr2LoCv0mPrnsw5vMbreP6OLApycpjdyKiIahY3tZkN2ymVepodXXP1dqkEJk\na0jWHromGSnKzhoNwqXRzPOu2733rHIDfLaOa3MCCEg83603qnZ6ITIjlGvWUagP\n4nhSYlOhAgMBAAECggEAPwZnDOGU7FgTOlSWYsClzXMK6XvcqkW+TqRKuS26jnTD\nsVii4yG8n3F3YQFT0bps9kj2SjlZsFROX1Rl9UYRDybkxGdaMec82hGPgjva1eSu\n9CU4rDere5i7p3SojnFEprvuBPN6EeToUT4sHXIHu94Wn60lqqjI6KoqkHZpkBnA\nJWvjoBWwl6uciZxk/k9yfZCH3+oLjCZmuePCDj3pKustzu/O5FE2Llw8dq8MEpCw\nMabK9x1SLd3Lw0rQkeWOG1B5bs5quYW/wwnKQ3tXjRXHjVhQqYVys2tHQelwIz5Z\n0v+X8U+KvamqSh4OsdZ3zhuo16Vc2sHEkgusRB18mwKBgQC2NhLtjSjlgCWRpWc5\nIlW9dhsL0zTPUOV3HjCd4bO/oUioJyidW1iP7/GrWvddzQMCy2cN0IsFHE8nk5sX\nVhbI2KHxmqH0/I6/9AOfhSaXaIBV0GwTCQcu9VyYa9O9YYQ2ItlTJZ397D6UFUDa\nrUDUnetuQ0KRtMqItTSmSmh7gwKBgQC1DTa056iT+aaQCimKCez5joHpujkUbcRi\ntQ+keK85UMu+JaFVIl0PsL+3OQ59YRZolMYqRu3B5pVpEdO0L3yitG8NG4OokCl9\nWmaOib1CkaiOoHizwJ16zsP3Rbmrn96L1uozW2OBvBZ7bfhffYIsw4G/tm9PzhFZ\nXVIZtQ7XCwKBgQCWIHV17kFM3kmV9+7Vfzmfkaj1FD7FtYRqoaAy5RyBfjRr/1+S\nTwnoBi1bHucDDQA3FDnDgxiXzFxXYmF9V7wMHZlIhDx+qnw/IaC+lSlXxI1Apyn0\ngRK/GQACwYzC047oP8xJmovTBAwHXH+D7Q2mYLryrU4y2P+qpYpMnK/7WwKBgD+C\nVBdfT74gi7HpLqsSUkM0HxQq08z7uDDbzJHoF6cmVWHs90vS0wwm9wlBhXirrt2e\nmmjIGqyywuRpcXa6VaEJZA8YALYHY9Zp+KG1ZWNNjvABEHYVcehbUViS589xM/Kf\nv+WmACDJJqXDbKWqdBhZuFDUoPVAlstfNeZ4oa0DAoGBAJP3pqRdtFe5l+xS96/Z\nLhLE+PRQTSwlSYtkju63h1Qy/APpl6Vve5crvOWpTc3Tx+FwRGzKk/L/jRp8FUmi\nwA2JTn9a/9Xns15t740tGrqsq7t4NSlKo0KZLBQ3ian3CCz41k0yRztrkEh9ZFPK\nOEAKkayeyBj+f+rWwpDIt9sv\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-fbsvc@aaos-assignment.iam.gserviceaccount.com",
+  "client_id": "115002391646449095117",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40aaos-assignment.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
     """.trimIndent()
-        val credentials =
-            GoogleCredentials.fromStream(serviceAccountJson.byteInputStream())
-                .createScoped(listOf("https://www.googleapis.com/auth/cloud-platform"))
+        val credentials = GoogleCredentials.fromStream(serviceAccountJson.byteInputStream())
+            .createScoped(listOf("https://www.googleapis.com/auth/cloud-platform"))
 
         return credentials.refreshAccessToken().tokenValue
     }
@@ -208,6 +235,31 @@ class SpeedService : CarAppService() {
         val notificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.notify(notificationId, notification.build())
+    }
+
+    private fun sendAwsSnsNotification(currentSpeed: Float) {
+        if (!isAwsInitialized || !::snsTopicArn.isInitialized) {
+            Log.e("AWS-SNS", "Cannot send notification - AWS not initialized")
+            return
+        }
+
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val message = JSONObject().apply {
+                    put("driver", name)
+                    put("currentSpeed", currentSpeed.toString())
+                    put("speedLimit", speedLimit.toString())
+                }.toString()
+
+                val publishRequest = PublishRequest().withTopicArn(snsTopicArn).withMessage(message)
+                    .withSubject("Speed Alert: Driver $name Exceeding Limit")
+
+                val publishResult = snsClient.publish(publishRequest)
+                Log.d("AWS-SNS", "Message sent with ID: ${publishResult.messageId}")
+            } catch (e: Exception) {
+                Log.e("AWS-SNS", "Failed to send SNS notification", e)
+            }
+        }
     }
 
     override fun onDestroy() {
